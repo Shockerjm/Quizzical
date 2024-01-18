fb09065510dfee787be7ef11d09d6b09452e3f55
Josh Miller <millertime_anytime@hotmail.com>
2024-01-18 15:49:57 -0600

    Update Question componenet

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,9 +1,11 @@
 import getApi from '../hooks/getApi.jsx'
 import he from 'he'
+import { useState } from 'react'
 
 function Question( { hide } ) {
 const api = 'https://opentdb.com/api.php?amount=5&difficulty=medium&type=multiple'
 const { data, loading, error } = getApi(api)
+const [ userResponse, setUserResponse ] = useState([])
 
 if (loading) {
   return <p style={{display: 'none'}}>Loading...</p>;
@@ -27,23 +29,75 @@ const group =  data.results.map((item) => {
     }
 
     return (
-      <>
-        <div key={item.question}>{he.decode(item.question)}</div>
-        {ansArr.map((ans) => {
-          return <p key={ans}>{ans}</p>
+      <div key={item.question} className="card">
+        <div>{he.decode(item.question)}</div>
+        {ansArr.map((ans, i) => {
+          return <button className="btn" key={ans} onClick={()=>collectAns(ans, he.decode(item.correct_answer), i)}>{ans}</button>
         })}
-      </>
+      </div>
       
     )
     
   })
+
+  function collectAns(ans, correct, index) {
+    if(userResponse.length === 0) {
+      return setUserResponse([{
+        correctAnswer: correct,
+        userAnswer: ans
+      }])
+    }
+
+    setUserResponse((prev) => {
+      if(prev.correctAnswer === correct) {
+        return {
+          ...prev,
+          userAnswer: ans
+        }
+      } else {
+        return [
+          ...prev,
+          {
+            correctAnswer: correct,
+            userAnswer: ans
+          }
+        ]
+      }
+    })
+
+    // setUserResponse(prev => {
+    //   if (prev.length === 0) {
+    //     return prev.push({
+    //       correctAnswer: correct,
+    //       userAnswer: ans
+    //     })
+    //   }
+
+
+      // if(prev.correctAnswer === correct) {
+      //     return {
+      //       ...prev, 
+      //       userAnswer: ans
+      //     }
+      //   } 
+
+
+      // return prev.push({
+      //   correctAnswer: correct,
+      //   userAnswer: ans
+      // })
+
+    // })
+
+  }
+
+  console.log(userResponse)
 
   return (
     <div className={hide ? 'hide' : ''}>
       {data && (
         <div>
           {group}  
-       
         </div>
       )}
     </div>
